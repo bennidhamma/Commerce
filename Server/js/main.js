e962827d82f835661899c1e0bfc662259d1c72eb
Benjamin Joldersma <ben@forgottenarts.com>
2013-05-04 11:43:02 -0700

    moving load friends into its own function

--- Server/js/main.js
+++ Server/js/main.js
@@ -27,4 +27,19 @@ function authenticate (plus) {
 			// list of games!
 		}
 	});
+	loadFriends();
+}
+
+function loadFriends () {
+  gapi.client.plus.people.list({
+		userId: 'me',
+		orderBy: 'best',
+		collection: 'visible'
+	}).execute(function(resp) {
+		for (var i = 0; i < resp.items.length; i++) {
+			var o = new Option(resp.items[i].displayName, resp.items[i].id);
+			o.label = resp.items[i].displayName;
+			$('#friends').append(o);
+		}
+	});
 }
