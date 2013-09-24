8f94cb47ae7670615cfa99d24dfdcb3b8448069b
Benjamin Joldersma <ben@forgottenarts.com>
2013-09-24 00:35:42 -0700

    add commercetests

---
+++ CommerceTests/TechnologyCardTests.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using ForgottenArts.Commerce;
+
+namespace Tests
+{
+	public class TechnologyCardTests : BaseTest
+	{
+		[Test]
+		public void CottonGinTest () {
+			CardCatalog catalog = new CardCatalog();
+			catalog.LoadCards("cards/trade", CardType.Trade);
+			catalog.LoadCards("cards/technology", CardType.Technology);
+
+			var cottonGin = catalog["Cotton Gin"];
+			var e = new ModifyTradeSetEvent () {
+				Good = "Cotton",
+				Size = 1
+			};
+			ScriptManager.Manager.ExecuteCardEvent (new Game(), cottonGin, null, "ModifyTradeSetEvent", e);
+
+			Assert.That(e.Size, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void PlayerCardEventsTest () {
+			Game g = new Game ();
+			var p1 = AddPlayer (g);
+			p1.TechnologyCards.Add ("Cotton Gin");
+
+			CardCatalog catalog = new CardCatalog();
+			catalog.LoadCards("cards/trade", CardType.Trade);
+			catalog.LoadCards("cards/technology", CardType.Technology);
+			GameRunner.Instance.Cards = catalog;
+			
+			var e = new ModifyTradeSetEvent () {
+				Good = "Cotton",
+				Size = 1
+			};
+			p1.HandleCardEvents (e);
+			
+			Assert.That(e.Size, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void FertilizersTest () {
+			Game g = new Game ();
+			var p1 = AddPlayer (g);
+			p1.AddHex (1, 1);
+			p1.TechnologyCards.Add ("Fertilizers");
+			
+			CardCatalog catalog = new CardCatalog();
+			catalog.LoadCards("cards/technology", CardType.Technology);
+			GameRunner.Instance.Cards = catalog;
+
+			Assert.That(p1.Hexes[0].GetPopulationLimit(), Is.EqualTo(2));
+		}
+	}
+}
---
+++ CommerceTests/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace CommerceTests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
